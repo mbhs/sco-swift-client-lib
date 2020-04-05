b24c08f97c458a11ff5a389e7fb47bc82a3793f8
Nonameentered <matthewshu88@gmail.com>
2020-04-05 01:10:48 -0400

    change date format

--- SCOAPIClient/Classes/OpenAPIs/Configuration.swift
+++ SCOAPIClient/Classes/OpenAPIs/Configuration.swift
@@ -6,11 +6,11 @@
 
 import Foundation
 
+//2020-04-02T21:32:10.930409-04:00
 open class Configuration {
 	
 	// This value is used to configure the date formatter that is used to serialize dates into JSON format. 
 	// You must set it prior to encoding any dates, and it will only be read once. 
 	@available(*, unavailable, message: "To set a different date format, use CodableHelper.dateFormatter instead.")
-    public static var dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
-    
+    public static var dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZZxxxxx"
 }
